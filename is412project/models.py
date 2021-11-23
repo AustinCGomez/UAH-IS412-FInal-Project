12f74dc97507028f6b1d443e02c8641ded7f5432
Austin Gomez <austincgomez@gmail.com>
2021-11-23 03:56:14 -0600

    Minor changes

--- is412project/models.py
+++ is412project/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Item(models.Model):
+    item_id = models.CharField(max_length=200)
+    item_name = models.CharField(max_length=200)
+    item_description = models.IntegerField() 
+    item_quantity = models.IntegerField()
+    item_price = models.IntegerField()
+
+
+
