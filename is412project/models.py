1d2740b55efc66a582effc5322025e2bd38d9d68
Austin Gomez <austincgomez@gmail.com>
2021-11-26 02:56:16 -0600

    Database item added.

--- is412project/models.py
+++ is412project/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 
 
+
+#This will create our database for the item system 
 class Item(models.Model):
-    item_id = models.CharField(max_length=200)
-    item_name = models.CharField(max_length=200)
-    item_description = models.IntegerField() 
-    item_quantity = models.IntegerField()
-    item_price = models.IntegerField()
-
-
+    item_id = models.CharField(max_length = 200)
+    item_name = models.CharField(max_length = 200)
+    item_description = models.CharField(max_length = 200)
+    item_quantity = models.IntegerField(default = 0)
+    item_price = models.IntegerField(default = 0)
 
