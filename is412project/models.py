60bf7826407f8ddd2412904f3d6b1734ef5dcb60
Austin Gomez <austincgomez@gmail.com>
2021-12-04 03:56:41 -0600

    This project adds some changes to the model and documentation

--- is412project/models.py
+++ is412project/models.py
@@ -4,9 +4,11 @@ from django.db import models
 
 #This will create our database for the item system 
 class Item(models.Model):
-    item_id = models.CharField(max_length = 200)
+    item_id = models.IntegerField(default = 0)
     item_name = models.CharField(max_length = 200)
     item_description = models.CharField(max_length = 200)
     item_quantity = models.IntegerField(default = 0)
     item_price = models.IntegerField(default = 0)
 
+    def __str__(self):
+        return self.item_name
--- is412project/views.py
+++ is412project/views.py
@@ -1,8 +1,23 @@
 from django.http import HttpResponse
+from . import urls
 from django.template import loader
 from django.shortcuts import render
+from .models import Item 
 
 def index(request):
-    template = loader.get_template('ecommerceproject/index.html')
-    return render(request, 'index.html')
+  #  obj = Item.objects.get(id=0)
+   # context = {
+    #    'title': obj.item_name,
+     #   'description': obj.item_description
+    #}
+    #context = {
+     #   'object': object
+    #}
+
+    items = Item.objects.all()
+    return render(request, 'ecommerceproject/index.html', {'items':items})
+
+#def index(request):
+ #   template = loader.get_template('ecommerceproject/index.html')
+  #  return render(request, 'index.html')
 
