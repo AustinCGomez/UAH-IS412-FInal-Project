9c74ce289b73761e5a3fbb301ad89ad022db9261
Austin Gomez <austincgomez@gmail.com>
2021-12-05 06:02:25 -0600

    Major Milestone 1 Frontend has functioning backend code!

--- is412project/models.py
+++ is412project/models.py
@@ -8,7 +8,7 @@ class Item(models.Model):
     item_name = models.CharField(max_length = 200)
     item_description = models.CharField(max_length = 200)
     item_quantity = models.IntegerField(default = 0)
-    item_price = models.IntegerField(default = 0)
+    item_price = models.DecimalField(max_digits=5, decimal_places=2)
 
     def __str__(self):
         return self.item_name
--- is412project/views.py
+++ is412project/views.py
@@ -5,17 +5,41 @@ from django.shortcuts import render
 from .models import Item 
 
 def index(request):
-  #  obj = Item.objects.get(id=0)
-   # context = {
-    #    'title': obj.item_name,
-     #   'description': obj.item_description
-    #}
+  alldata = Item.objects.all()
+  context = {'alldata':alldata}
+  
+  return render(request,'ecommerceproject/index.html', context)
+
+
+#def index(request):
+ # obj = Item.objects.get(id=1)
+ #obj2 = Item.objects.get(id=2)
+ # context = {
+  #  'title':obj.item_name,
+  #  'description': obj.item_description,
+  #  'price': obj.item_price,
+  #  'quantity': obj.item_quantity
+   
+ # }
+ # return render(request,'ecommerceproject/index.html', context)
+
+#def index2(request):
+ # obj = Item.objects.get(id=1)
+ # context2 = {
+ #   'title2':obj.item_name,
+  #  'description2': obj.item_description,
+  #  'price2': obj.item_price,
+  #  'quantity2': obj.item_quantity
+
+ # }
+ # return render(request,'ecommerceproject/index.html', context2)
+
     #context = {
      #   'object': object
     #}
 
-    items = Item.objects.all()
-    return render(request, 'ecommerceproject/index.html', {'items':items})
+    #items = Item.objects.all()
+    #return render(request, 'ecommerceproject/index.html', {'items':items})
 
 #def index(request):
  #   template = loader.get_template('ecommerceproject/index.html')
